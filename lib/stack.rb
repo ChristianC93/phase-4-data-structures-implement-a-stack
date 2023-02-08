0ba5f07ef5e1671fc427efa777e0ada217a0b0f8
Christian Castillo <castillochristian3@gmail.com>
2023-02-08 11:00:56 -0500

    commit tree 1599225881437f33f880f565d320fe1b36faf270

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,37 @@
-# your code here
+class Stack
+    attr_reader :limit
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(val)
+        @stack.size == limit ? raise("Stack Overflow") : @stack.push(val)
+    end
+
+    def pop()
+        val = @stack.pop()
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @stack.size == limit ? true : false
+    end
+
+    def search(val)
+        last = @stack.size - 1
+        @stack.index(val) ? last - @stack.index(val) : -1
+    end
+end
